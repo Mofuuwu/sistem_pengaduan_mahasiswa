9454838c37bd637496a52e3839f1b6c14ef26b4a
Mofuuwu <Jeangunhildr26@gmail.com>
2025-01-30 09:55:08 +0700

    menambahkan tab aduan yg sudah selesai dan aduan yg belum selesai pada halaman aduan yg diambil alih pada dashbboard petugas

--- app/Filament/Employee/Resources/ComplaintBeingWorkedResource/Pages/ListComplaintBeingWorkeds.php
+++ app/Filament/Employee/Resources/ComplaintBeingWorkedResource/Pages/ListComplaintBeingWorkeds.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Employee\Resources\ComplaintBeingWorkedResource\Pages;
 
-use App\Filament\Employee\Resources\ComplaintBeingWorkedResource;
 use Filament\Actions;
+use App\Models\Complaint;
+use Illuminate\Support\Facades\Auth;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Employee\Resources\ComplaintBeingWorkedResource;
 
 class ListComplaintBeingWorkeds extends ListRecords
 {
@@ -13,6 +17,70 @@ class ListComplaintBeingWorkeds extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+        ];
+    }
+    public function getTabs(): array
+    {
+        return [
+            
+            'semua' => Tab::make('Semua')
+            ->icon('heroicon-s-square-3-stack-3d')
+            ->badge(
+                badge: function() {
+                    return Complaint::whereHas('logs', function (Builder $query) {
+                        $query->where('employee_id', Auth::user()->employee->id)
+                            ->whereIn('id', function ($query) {
+                                $query->selectRaw('max(id)')
+                                      ->from('logs')
+                                      ->groupBy('complaint_id');
+                            });
+                    })->count();
+                }
+            ), 
+            'belum selesai' => Tab::make('Belum Delesai')
+            ->icon('heroicon-s-x-circle')
+            ->badge(function() {
+                return Complaint::whereHas('logs', function (Builder $query) {
+                    $query->whereNot('name', 'selesai')->where('employee_id', Auth::user()->employee->id)
+                        ->whereIn('id', function ($query) {
+                            $query->selectRaw('max(id)')
+                                  ->from('logs')
+                                  ->groupBy('complaint_id');
+                        });
+                })->count();
+            })
+            ->modifyQueryUsing(function (Builder $query) {
+                $query->whereHas('logs', function (Builder $query) {
+                    $query->whereNot('name', 'selesai')->where('employee_id', Auth::user()->employee->id)
+                        ->whereIn('id', function ($query) {
+                            $query->selectRaw('max(id)')
+                                  ->from('logs')
+                                  ->groupBy('complaint_id');
+                        });
+                });
+            }),
+            'selesai' => Tab::make('Selesai')
+            ->icon('heroicon-s-check-circle')
+            ->badge(function() {
+                return Complaint::whereHas('logs', function (Builder $query) {
+                    $query->where('name', 'selesai')
+                        ->whereIn('id', function ($query) {
+                            $query->selectRaw('max(id)')
+                                  ->from('logs')
+                                  ->groupBy('complaint_id');
+                        });
+                })->count();
+            })
+            ->modifyQueryUsing(function (Builder $query) {
+                $query->whereHas('logs', function (Builder $query) {
+                    $query->where('name', 'selesai')
+                        ->whereIn('id', function ($query) {
+                            $query->selectRaw('max(id)')
+                                  ->from('logs')
+                                  ->groupBy('complaint_id');
+                        });
+                });
+            }),
         ];
     }
 }
